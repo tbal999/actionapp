edc44dca2936382ad6c898bfaddae60f67a165aa
tbal999 <53711814+tbal999@users.noreply.github.com>
2021-04-27 20:26:10 +0100

    Update actionapp.go

--- actionapp.go
+++ actionapp.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 )
+
+// iterate through a bunch of files - filepath fed via argument 1
+func iterfiles(path string) {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+	fmt.Println("Complete")
+}
 
 func main() {
 	commands := os.Args[1:]
 	fmt.Println("You have taken in these args:")
 	fmt.Println(commands)
+	iterfiles(os.Args[1])
 }
